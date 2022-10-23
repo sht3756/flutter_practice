c03e2f80541a01db330e71f8649abb972e9fd2f2
SHT-3756 <gkdlfndkekd@naver.com>
2022-10-23 23:24:29 +0900

    first flutter

---
+++ hello_world_first/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(
+          child: Text(
+              'Hello World!',
+            style: TextStyle(
+              color: Colors.white
+            ),
+          )
+        )
+      )
+    )
+  );
+}
+

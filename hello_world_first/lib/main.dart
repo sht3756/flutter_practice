a187556240c1db039444c48ad47b05934b85f8bc
SHT-3756 <gkdlfndkekd@naver.com>
2022-10-24 18:35:31 +0900

    가운데 정렬 및 로딩 인디케이터 추가

--- hello_world_first/lib/main.dart
+++ hello_world_first/lib/main.dart
@@ -11,8 +11,10 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Image.asset('asset/img/logo.png'),
+          CircularProgressIndicator(),
         ],
       ),
     );

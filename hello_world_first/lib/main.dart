93d282b04e6dae4e43d006398aed41cac3d1a182
SHT-3756 <gkdlfndkekd@naver.com>
2022-10-24 23:49:29 +0900

    로딩 인디케이터 스타일 추가

--- hello_world_first/lib/main.dart
+++ hello_world_first/lib/main.dart
@@ -10,11 +10,14 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.orange,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Image.asset('asset/img/logo.png'),
-          CircularProgressIndicator(),
+          CircularProgressIndicator(
+            color: Colors.white,
+          ),
         ],
       ),
     );

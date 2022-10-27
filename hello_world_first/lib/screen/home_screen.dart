e7d18fe7f9e5aa15692bd918ba68f821eb6381b9
SHT-3756 <gkdlfndkekd@naver.com>
2022-10-27 23:38:09 +0900

    height 와 width 제거 및 mainAxisSize 사용

--- hello_world_first/lib/screen/home_screen.dart
+++ hello_world_first/lib/screen/home_screen.dart
@@ -11,8 +11,6 @@ class HomeScreen extends StatelessWidget {
         bottom: false,
         child: Container(
           color: Colors.black,
-          // width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
           child: Row(
             // MainAxisAlignment - 주죽 정렬
             // start - 시작
@@ -21,14 +19,19 @@ class HomeScreen extends StatelessWidget {
             // spaceBetween - 위젯들 사이의 간격이 동일하게 배치
             // spaceEvenly - 위젯들 사이의 간격 동일하게 배치되면서 끝과 끝에는 위젯이 아닌 빈 간격으로 배치
             // spaceAround - spaceEvenly + 끝과 끝의 간격 1/2
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisAlignment: MainAxisAlignment.start,
 
             // CrossAxisAlignment - 반대축 정렬
             // start - 시작
             // end - 끝
             // center - 가운데
             // stretch - 최대한으로 늘린다.
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+            crossAxisAlignment: CrossAxisAlignment.start,
+
+            // MainAxisSize 는 주축 크기 조정
+            // max - 최대
+            // min - 최소
+            mainAxisSize: MainAxisSize.max,
             children: [
               Container(
                 color: Colors.red,

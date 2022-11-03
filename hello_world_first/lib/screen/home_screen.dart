f6d30ffdeff55b42968f48d3c2e8be935b5ff65e
SHT-3756 <gkdlfndkekd@naver.com>
2022-11-03 23:08:46 +0900

    Life Cycle 확인

--- hello_world_first/lib/screen/home_screen.dart
+++ hello_world_first/lib/screen/home_screen.dart
@@ -190,52 +190,7 @@ import 'package:webview_flutter/webview_flutter.dart';
 // }
 // blog 웹앱 만들기 끝
 
-// class HomeScreen extends StatefulWidget {
-//   final Color color;
-//
-//   const HomeScreen({
-//     required this.color,
-//     Key? key,
-//   }) : super(key: key)
-//
-//
-//   @override
-//   State<StatefulWidget> createState() {
-//     return _HomeScreenState();
-//   }
-// }
-//
-// class _HomeScreenState extends State<HomeScreen> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       width: 50.0,
-//       height: 50.0,
-//       color: widget.color,
-//     );
-//   }
-// }
-
-class HomeScreen extends StatefulWidget {
-  final Color color;
-
-  const HomeScreen({required this.color, Key? key}) : super(key: key);
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 50.0,
-      height: 50.0,
-      color: widget.color,
-    );
-  }
-}
-
+// StatelessWidget 만들기 시작
 // class _HomeScreen extends StatelessWidget {
 //   final Color color;
 //
@@ -253,3 +208,75 @@ class _HomeScreenState extends State<HomeScreen> {
 //     );
 //   }
 // }
+// StatelessWidget 만들기 끝
+
+// StatefulWidget 만들기 시작
+class HomeScreen extends StatefulWidget {
+  final Color color;
+
+  HomeScreen({required this.color, Key? key}) : super(key: key) {
+    print('Widget Constructor 실행!');
+  }
+
+  @override
+  State<HomeScreen> createState() {
+    print('createState 실행!');
+    return _HomeScreenState();
+  }
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int number = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    print('initState 실행!');
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    print('didChangeDependencies 실행!');
+  }
+
+  @override
+  void deactivate() {
+    print('deactivate 실행!');
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    print('dispose 실행!');
+    super.dispose();
+  }
+
+  @override
+  void didUpdateWidget(covariant HomeScreen oldWidget) {
+    print('didUpdateWidget 실행!');
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('build 실행!');
+
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          number ++;
+        });
+
+      },
+      child: Container(
+        width: 50.0,
+        height: 50.0,
+        color: widget.color,
+        child: Center(child: Text(number.toString())),
+      ),
+    );
+  }
+}
+// StatefulWidget 만들기 끝

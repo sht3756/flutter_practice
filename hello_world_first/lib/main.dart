b74e727dbdeb4ef0d9e3a723140df3a0f4ab6d5b
SHT-3756 <gkdlfndkekd@naver.com>
2022-11-04 21:46:06 +0900

    이미지 추가 및 이미지 슬라이드되게 적용

--- hello_world_first/lib/main.dart
+++ hello_world_first/lib/main.dart
@@ -39,77 +39,81 @@ import 'package:hello_world_first/screen/home_screen.dart';
 // Column Row 끝
 
 // StatefulWidget 시작
-void main() {
-  runApp(MaterialApp(
-    home: Root(),
-  ));
-}
-
-class Root extends StatefulWidget {
-  const Root({Key? key}) : super(key: key);
-
-  @override
-  _RootState createState() => _RootState();
-}
-
-class _RootState extends State<Root> {
-  Color color = Colors.blue;
-  bool show = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: Column(
-            children: [
-              Expanded(
-                child: Center(
-                  child: show
-                      ? HomeScreen(
-                    color: color,
-                  )
-                      : Container(),
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        color = color == Colors.red ? Colors.blue : Colors.red;
-                      });
-                    },
-                    child: Text(
-                      '색깔 변경하기',
-                    ),
-                  ),
-                  const SizedBox(height: 4.0),
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        show = !show;
-                      });
-                    },
-                    child: Text(
-                      !show ? '위젯 생성하기' : '위젯 삭제하기',
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-
+// void main() {
+//   runApp(MaterialApp(
+//     home: Root(),
+//   ));
+// }
+//
+// class Root extends StatefulWidget {
+//   const Root({Key? key}) : super(key: key);
+//
+//   @override
+//   _RootState createState() => _RootState();
+// }
+//
+// class _RootState extends State<Root> {
+//   Color color = Colors.blue;
+//   bool show = false;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: SafeArea(
+//         child: Padding(
+//           padding: const EdgeInsets.symmetric(horizontal: 8.0),
+//           child: Column(
+//             children: [
+//               Expanded(
+//                 child: Center(
+//                   child: show
+//                       ? HomeScreen(
+//                     color: color,
+//                   )
+//                       : Container(),
+//                 ),
+//               ),
+//               Column(
+//                 crossAxisAlignment: CrossAxisAlignment.stretch,
+//                 children: [
+//                   ElevatedButton(
+//                     onPressed: () {
+//                       setState(() {
+//                         color = color == Colors.red ? Colors.blue : Colors.red;
+//                       });
+//                     },
+//                     child: Text(
+//                       '색깔 변경하기',
+//                     ),
+//                   ),
+//                   const SizedBox(height: 4.0),
+//                   ElevatedButton(
+//                     onPressed: () {
+//                       setState(() {
+//                         show = !show;
+//                       });
+//                     },
+//                     child: Text(
+//                       !show ? '위젯 생성하기' : '위젯 삭제하기',
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 // StatefulWidget 끝
-
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    )
+  );
+}
 
 
 
--- hello_world_first/lib/screen/home_screen.dart
+++ hello_world_first/lib/screen/home_screen.dart
@@ -211,72 +211,95 @@ import 'package:webview_flutter/webview_flutter.dart';
 // StatelessWidget 만들기 끝
 
 // StatefulWidget 만들기 시작
-class HomeScreen extends StatefulWidget {
-  final Color color;
+// class HomeScreen extends StatefulWidget {
+//   final Color color;
+//
+//   HomeScreen({required this.color, Key? key}) : super(key: key) {
+//     print('Widget Constructor 실행!');
+//   }
+//
+//   @override
+//   State<HomeScreen> createState() {
+//     print('createState 실행!');
+//     return _HomeScreenState();
+//   }
+// }
+//
+// class _HomeScreenState extends State<HomeScreen> {
+//   int number = 0;
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//
+//     print('initState 실행!');
+//   }
+//
+//   @override
+//   void didChangeDependencies() {
+//     super.didChangeDependencies();
+//     print('didChangeDependencies 실행!');
+//   }
+//
+//   @override
+//   void deactivate() {
+//     print('deactivate 실행!');
+//     super.deactivate();
+//   }
+//
+//   @override
+//   void dispose() {
+//     print('dispose 실행!');
+//     super.dispose();
+//   }
+//
+//   @override
+//   void didUpdateWidget(covariant HomeScreen oldWidget) {
+//     print('didUpdateWidget 실행!');
+//     super.didUpdateWidget(oldWidget);
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     print('build 실행!');
+//
+//     return GestureDetector(
+//       onTap: () {
+//         setState(() {
+//           number ++;
+//         });
+//
+//       },
+//       child: Container(
+//         width: 50.0,
+//         height: 50.0,
+//         color: widget.color,
+//         child: Center(child: Text(number.toString())),
+//       ),
+//     );
+//   }
+// }
+// StatefulWidget 만들기 끝
 
-  HomeScreen({required this.color, Key? key}) : super(key: key) {
-    print('Widget Constructor 실행!');
-  }
+// 전자 액자 만들기 시작
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  State<HomeScreen> createState() {
-    print('createState 실행!');
-    return _HomeScreenState();
-  }
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int number = 0;
-
-  @override
-  void initState() {
-    super.initState();
-
-    print('initState 실행!');
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    print('didChangeDependencies 실행!');
-  }
-
-  @override
-  void deactivate() {
-    print('deactivate 실행!');
-    super.deactivate();
-  }
-
-  @override
-  void dispose() {
-    print('dispose 실행!');
-    super.dispose();
-  }
-
-  @override
-  void didUpdateWidget(covariant HomeScreen oldWidget) {
-    print('didUpdateWidget 실행!');
-    super.didUpdateWidget(oldWidget);
-  }
-
   @override
   Widget build(BuildContext context) {
-    print('build 실행!');
-
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          number ++;
-        });
-
-      },
-      child: Container(
-        width: 50.0,
-        height: 50.0,
-        color: widget.color,
-        child: Center(child: Text(number.toString())),
+    return Scaffold(
+      body: PageView(
+        children: [2,3,4].map(
+            (e) => Image.asset('asset/img/img$e.HEIC', fit: BoxFit.cover)
+        ).toList(),
       ),
     );
   }
 }
-// StatefulWidget 만들기 끝
+
+// 전자 액자 만들기 끝

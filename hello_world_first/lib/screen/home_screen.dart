1ab0404deebea1e672af0e33eab9a03c97544619
SHT-3756 <gkdlfndkekd@naver.com>
2022-11-06 23:55:40 +0900

    코드 리팩토링 및 디자인 추가

--- hello_world_first/lib/screen/home_screen.dart
+++ hello_world_first/lib/screen/home_screen.dart
@@ -357,44 +357,81 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
         backgroundColor: Colors.pink[100],
         body: SafeArea(
+          bottom: false,
           child: Container(
             width: MediaQuery.of(context).size.width,
             child: Column(
               children: [
-                Text(
-                  'U&I',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'parisienne',
-                    fontSize: 80.0,
-                  ),
-                ),
-                Text(
-                  '우리 처음 만난 날',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'sunflower',
-                      fontSize: 30.0),
-                ),
-                Text(
-                  '2020.01.01',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'sunflower',
-                      fontSize: 20.0),
-                ),
-                IconButton(onPressed: () {}, icon: Icon(Icons.favorite)),
-                Text(
-                  'D+1',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'sunflower',
-                      fontWeight: FontWeight.w700,
-                      fontSize: 50.0),
-                )
+                _TopPart(),
+                _BottomPart(),
               ],
             ),
           ),
         ));
+  }
+}
+
+class _TopPart extends StatelessWidget {
+  const _TopPart({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            'U&I',
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'parisienne',
+              fontSize: 80.0,
+            ),
+          ),
+          Column(
+            children: [
+              Text(
+                '우리 처음 만난 날',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'sunflower',
+                    fontSize: 30.0),
+              ),
+              Text(
+                '2020.01.01',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'sunflower',
+                    fontSize: 20.0),
+              ),
+            ],
+          ),
+          IconButton(
+              iconSize: 60.0,
+              onPressed: () {},
+              icon: Icon(
+                Icons.favorite,
+                color: Colors.red,
+              )),
+          Text(
+            'D+1',
+            style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'sunflower',
+                fontWeight: FontWeight.w700,
+                fontSize: 50.0),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _BottomPart extends StatelessWidget {
+  const _BottomPart({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(child: Image.asset('asset/img/middle_image.png'));
   }
 }

9346e8992ade6c772f940a5b19911cbbefeb287d
SHT-3756 <gkdlfndkekd@naver.com>
2022-12-07 15:38:46 +0900

    feat: padding 적용 및 constant color 생성

--- hello_world_first/lib/random_number_study/random_number.dart
+++ hello_world_first/lib/random_number_study/random_number.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world_first/random_number_study/constant/color.dart';
 
 class RandomNumberPage extends StatefulWidget {
   const RandomNumberPage({Key? key}) : super(key: key);
@@ -11,36 +12,51 @@ class _RandomNumberPageState extends State<RandomNumberPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: PRIMARY_COLOR,
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('랜덤함수 생성기'),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.settings,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '랜덤함수 생성기',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.w700),
                   ),
-                ),
-              ],
-            ),
-            Expanded(
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.settings,
+                      color: RED_COLOR,
+                    ),
+                  ),
+                ],
+              ),
+              Expanded(
                 child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('123'),
-                Text('223'),
-                Text('332'),
-              ],
-            )),
-            // 무한대값 double.infinity
-            SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(onPressed: () {}, child: Text('생성하기!')))
-          ],
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('123'),
+                    Text('223'),
+                    Text('332'),
+                  ],
+                ),
+              ),
+              // 무한대값 double.infinity
+              SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(backgroundColor: RED_COLOR),
+                      onPressed: () {},
+                      child: Text('생성하기!')))
+            ],
+          ),
         ),
       ),
     );

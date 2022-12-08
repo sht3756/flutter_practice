5631de627a9ec3b847b493420e1659e50321e5bb
SHT-3756 <gkdlfndkekd@naver.com>
2022-12-08 21:07:41 +0900

    refactor: settings_screen.dart_코드 정리

--- hello_world_first/lib/random_number_study/screen/settings_screen.dart
+++ hello_world_first/lib/random_number_study/screen/settings_screen.dart
@@ -22,41 +22,91 @@ class _SettingScreenState extends State<SettingScreen> {
             // stretch 만 넣어도 버튼이 커지지만, SafeArea를 통해서 하단바에 안붙게 적용
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Expanded(
-                child: Row(
-                  children: maxNumber
-                      .toInt()
-                      .toString()
-                      .split('')
-                      .map((e) => Image.asset(
-                            'asset/img/random_number/$e.png',
-                            width: 50.0,
-                            height: 70.0,
-                          ))
-                      .toList(),
-                ),
+              _Body(
+                maxNumber: maxNumber,
               ),
-              Slider(
-                value: maxNumber,
-                min: 1000,
-                max: 100000,
-                onChanged: (double val) {
-                  setState(() {
-                    maxNumber = val;
-                  });
-                },
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pop(maxNumber.toInt());
-                },
-                child: Text('저장!'),
-                style: ElevatedButton.styleFrom(backgroundColor: RED_COLOR),
-              )
+              _Footer(
+                  onSliderChanged: onSliderChanged,
+                  onButtonPressed: onButtonPressed,
+                  maxNumber: maxNumber),
             ],
           ),
         ),
       ),
+    );
+  }
+
+
+  void onSliderChanged (double val) {
+    setState(() {
+      maxNumber = val;
+    });
+  }
+
+  void onButtonPressed () {
+    Navigator.of(context).pop(maxNumber.toInt());
+  }
+
+}
+
+
+
+
+class _Body extends StatelessWidget {
+  final double maxNumber;
+
+  const _Body({required this.maxNumber, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Row(
+        children: maxNumber
+            .toInt()
+            .toString()
+            .split('')
+            .map((e) => Image.asset(
+                  'asset/img/random_number/$e.png',
+                  width: 50.0,
+                  height: 70.0,
+                ))
+            .toList(),
+      ),
+    );
+  }
+}
+
+class _Footer extends StatelessWidget {
+  final ValueChanged<double>? onSliderChanged;
+  final VoidCallback onButtonPressed;
+  final double maxNumber;
+
+  const _Footer(
+      {required this.onSliderChanged,
+      required this.onButtonPressed,
+      required this.maxNumber,
+      Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Slider(
+          value: maxNumber,
+          min: 1000,
+          max: 100000,
+          onChanged: onSliderChanged,
+
+
+        ),
+        ElevatedButton(
+          onPressed: onButtonPressed,
+          child: Text('저장!'),
+          style: ElevatedButton.styleFrom(backgroundColor: RED_COLOR),
+        )
+      ],
     );
   }
 }

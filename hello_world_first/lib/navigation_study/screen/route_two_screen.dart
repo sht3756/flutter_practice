2c9c3fb9ad761cab00b2715cbcdc11682be6e7c4
SHT-3756 <gkdlfndkekd@naver.com>
2022-12-12 23:55:22 +0900

    feat: route_two_screen.dart_라우트 경로 설정, arguments 넘기기, 라우트 기록 삭제

--- hello_world_first/lib/navigation_study/screen/route_two_screen.dart
+++ hello_world_first/lib/navigation_study/screen/route_two_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hello_world_first/navigation_study/layout/main_layout.dart';
+import 'package:hello_world_first/navigation_study/screen/route_three_screen.dart';
 
 class RouterTwoScreen extends StatelessWidget {
   const RouterTwoScreen({Key? key}) : super(key: key);
@@ -23,6 +24,31 @@ class RouterTwoScreen extends StatelessWidget {
             Navigator.of(context).pushNamed('/three', arguments: 999);
           },
           child: Text('Push Named')),
+      ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (_) => RouterThreeScreen()));
+          },
+          child: Text('Push Replacment')),
+      ElevatedButton(
+          // pushAndRemoveUntil
+          // (route) => false) 기존에 존재하는 라우트를 기록을 삭제 해준다.
+          // (route) => true) 를 하면 라우트된 기록을 살린다.
+          onPressed: () {
+            Navigator.of(context).pushAndRemoveUntil(
+                MaterialPageRoute(builder: (_) => RouterThreeScreen()),
+                (route) => false);
+          },
+          child: Text('Push And Remove Until')),
+      ElevatedButton(
+          onPressed: () {
+            // 특정 라우트만 삭제 할수 있다.
+
+            Navigator.of(context).pushAndRemoveUntil(
+                MaterialPageRoute(builder: (_) => RouterThreeScreen()),
+                (route) => route.settings.name == '/');
+          },
+          child: Text('Push And Remove Until'))
     ]);
   }
 }

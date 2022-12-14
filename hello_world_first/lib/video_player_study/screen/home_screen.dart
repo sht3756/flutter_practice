e82861149fdcb3f6b28d9e5849b0b71bb7538ea4
SHT-3756 <gkdlfndkekd@naver.com>
2022-12-14 16:13:28 +0900

    feat: home_screen.dart_홈스크린 UI 작업

--- hello_world_first/lib/video_player_study/screen/home_screen.dart
+++ hello_world_first/lib/video_player_study/screen/home_screen.dart
@@ -5,10 +5,51 @@ class VideoHomeScreenPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('homeScreen'),
-      ),
+    // 공통적인 textStyle 변수선언
+    final textStyle = TextStyle(
+      color: Colors.white,
+      fontSize: 30.0,
+      fontWeight: FontWeight.w300,
     );
+
+    return Scaffold(
+        body: Container(
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          Color(0xFF2A3A7C),
+          Color(0xFF000118),
+        ],
+      )),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image(
+            image: AssetImage("asset/img/logo.png"),
+          ),
+          SizedBox(
+            height: 30.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'VIDEO',
+                style: textStyle,
+              ),
+              Text(
+                'PLAYER',
+                style: textStyle.copyWith(
+                    fontWeight:
+                        FontWeight.w700), // 공통 스타일과 다른 부분은 copyWith() 사용
+              )
+            ],
+          )
+        ],
+      ),
+    ));
   }
 }

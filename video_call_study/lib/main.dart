734f509b322ea6477e25aefa8ce3b2919df384e3
SHT-3756 <gkdlfndkekd@naver.com>
2022-12-21 19:35:50 +0900

    feat: home_screen.dart-UI 작업

--- video_call_study/lib/main.dart
+++ video_call_study/lib/main.dart
@@ -4,6 +4,9 @@ import 'package:video_call_study/screen/home_screen.dart';
 void main() {
   runApp(
       MaterialApp(
+        theme: ThemeData(
+          fontFamily: 'NotoSans',
+        ),
         home: HomeScreen(),
       )
   );

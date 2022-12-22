1f59a47cf8faf206a9b572c4cbcedfb1288c8c00
SHT-3756 <gkdlfndkekd@naver.com>
2022-12-22 19:15:20 +0900

    feat: 프로젝트 세팅(캘린더)

---
+++ scheduler_study/android/app/src/main/kotlin/com/example/scheduler_study/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.scheduler_study
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ scheduler_study/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:scheduler_study/screen/home_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData(
+      fontFamily: 'NotoSans'
+    ),
+    home: HomeScreen(),
+  ));
+}
+

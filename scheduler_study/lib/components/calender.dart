2e9fdf49a7ddc8d525b610813a8fa4f40a37d82e
SHT-3756 <gkdlfndkekd@naver.com>
2022-12-22 20:38:03 +0900

    feat: calender.dart_Table Calender 스타일링 ,feat: constant 폴더 생성(메인 컬러)

--- scheduler_study/lib/components/calender.dart
+++ scheduler_study/lib/components/calender.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:scheduler_study/constant/colors.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class Calender extends StatefulWidget {
@@ -13,6 +14,16 @@ class _CalenderState extends State<Calender> {
 
   @override
   Widget build(BuildContext context) {
+    final defaultBoxDecoration = BoxDecoration(
+      color: Colors.grey[200],
+      borderRadius: BorderRadius.circular(8.0),
+    );
+
+    final defaultTextStyle = TextStyle(
+      color: Colors.grey[600],
+      fontWeight: FontWeight.w700,
+    );
+
     return TableCalendar(
       focusedDay: DateTime.now(),
       firstDay: DateTime(1800),
@@ -29,6 +40,27 @@ class _CalenderState extends State<Calender> {
           fontSize: 16.0,
         ),
       ),
+      calendarStyle: CalendarStyle(
+          // 오늘 날짜 보여주기 여부
+          isTodayHighlighted: false,
+          // 평일 꾸미기
+          defaultDecoration: defaultBoxDecoration,
+          // 주말 꾸미기
+          weekendDecoration: defaultBoxDecoration,
+          // 선택한 날 꾸미기
+          selectedDecoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(8.0),
+              border: Border.all(
+                color: PRIMARY_COLOR,
+                width: 1.0,
+              )),
+          // 평일 텍스트 스타일
+          defaultTextStyle: defaultTextStyle,
+          // 주말 택스트 스타일
+          weekendTextStyle: defaultTextStyle,
+          // 선택 날 텍스트 스타일
+          selectedTextStyle: defaultTextStyle.copyWith(color: PRIMARY_COLOR)),
       // 특정날짜를 선택할때 실행!
       onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
         print('selectedDay : $selectedDay');

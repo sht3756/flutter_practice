8bbd70a04e01d90b24767c22dedc2cdc56c1cbff
SHT-3756 <gkdlfndkekd@naver.com>
2022-12-23 21:50:04 +0900

    feat: TextField 마무리 하기(분기처리)

--- scheduler_study/lib/components/new_schedule_bottom_sheet.dart
+++ scheduler_study/lib/components/new_schedule_bottom_sheet.dart
@@ -10,30 +10,37 @@ class ScheduleBottomSheet extends StatelessWidget {
     // 시스템적으로 가려진 부분
     final bottomInset = MediaQuery.of(context).viewInsets.bottom;
 
-    return Container(
-      height: MediaQuery.of(context).size.height / 2 + bottomInset,
-      color: Colors.white,
-      child: Padding(
-        padding: EdgeInsets.only(bottom: bottomInset),
-        child: Padding(
-          padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              _Time(),
-              SizedBox(
-                height: 16.0,
+    return GestureDetector(
+      onTap: (){
+        FocusScope.of(context).requestFocus(FocusNode());
+      },
+      child: SafeArea(
+        child: Container(
+          height: MediaQuery.of(context).size.height / 2 + bottomInset,
+          color: Colors.white,
+          child: Padding(
+            padding: EdgeInsets.only(bottom: bottomInset),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _Time(),
+                  SizedBox(
+                    height: 16.0,
+                  ),
+                  _Content(),
+                  SizedBox(
+                    height: 16.0,
+                  ),
+                  _ColorPicker(),
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  _SaveButton(),
+                ],
               ),
-              _Content(),
-              SizedBox(
-                height: 16.0,
-              ),
-              _ColorPicker(),
-              SizedBox(
-                height: 8.0,
-              ),
-              _SaveButton(),
-            ],
+            ),
           ),
         ),
       ),
@@ -50,14 +57,14 @@ class _Time extends StatelessWidget {
       children: [
         Expanded(
             child: CustomTextField(
-          label: '시작 시간',
+          label: '시작 시간', isTime: true,
         )),
         SizedBox(
           width: 16.0,
         ),
         Expanded(
             child: CustomTextField(
-          label: '마감 시간',
+          label: '마감 시간', isTime: true,
         )),
       ],
     );
@@ -69,8 +76,10 @@ class _Content extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomTextField(
-      label: '내용',
+    return Expanded(
+      child: CustomTextField(
+        label: '내용', isTime: false,
+      ),
     );
   }
 }
--- scheduler_study/lib/components/text_field.dart
+++ scheduler_study/lib/components/text_field.dart
@@ -1,30 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:scheduler_study/constant/colors.dart';
 
 class CustomTextField extends StatelessWidget {
   final String label;
 
-  const CustomTextField({Key? key, required this.label}) : super(key: key);
+  // true - 시간 / false - 내용
+  final bool isTime;
+
+  const CustomTextField({Key? key, required this.label, required this.isTime})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(label, style: TextStyle(
-          color: PRIMARY_COLOR,
-          fontWeight: FontWeight.w600
-        ),),
-        TextField(
-          cursorColor: Colors.grey,
-          decoration: InputDecoration(
-            // 테두리 삭제
-              border: InputBorder.none,
-              filled: true,
-              fillColor: Colors.grey[300]
-          ),
+        Text(
+          label,
+          style: TextStyle(color: PRIMARY_COLOR, fontWeight: FontWeight.w600),
         ),
+        if(isTime) renderTextField(),
+        if(!isTime) Expanded(child: renderTextField()),
       ],
     );
   }
+  Widget renderTextField(){
+    return TextField(
+      cursorColor: Colors.grey,
+      maxLines: isTime ? 1 : null,
+      expands: !isTime,
+      keyboardType: isTime ? TextInputType.number : TextInputType.multiline,
+      inputFormatters:
+      isTime ? [FilteringTextInputFormatter.digitsOnly] : [],
+      decoration: InputDecoration(
+        // 테두리 삭제
+          border: InputBorder.none,
+          filled: true,
+          fillColor: Colors.grey[300]),
+    );
+  }
 }
+

f5e96cde5022c1ba8302a99e36f841f7a6120f6b
SHT-3756 <gkdlfndkekd@naver.com>
2022-12-25 23:40:51 +0900

    feat: flutter 의 특별한 기능-CodeGeneration

--- scheduler_study/lib/database/drift_database.dart
+++ scheduler_study/lib/database/drift_database.dart
@@ -17,19 +17,35 @@ part 'drift_database.g.dart';
     CategoryColors,
   ],
 )
-
 //  _$LocalDatabase 는 현재 이 클래스에서 선언해놓지 않았다. 하지만 사용가능한 이유는 part 로 불러왔기 떄문이다.
 class LocalDatabase extends _$LocalDatabase {
   LocalDatabase() : super(_openConnection());
+
+  // 스케쥴 추가 insert 문
+  Future<int> createSchedule(SchedulesCompanion data) =>
+      into(schedules).insert(data);
+
+  // 색상 추가 insert 문
+  Future<int> createCategoryColor(CategoryColorsCompanion data) =>
+      into(categoryColors).insert(data);
+
+  // 카테고리 컬러 다 불러오는 select 문
+  Future<List<CategoryColor>> getCategoryColors() =>
+      select(categoryColors).get();
+
+
+  @override
+  int get schemaVersion => 1;
+// int get schemaVersion => throw UnimplementedError();
 }
 
 LazyDatabase _openConnection() {
-return LazyDatabase(() async {
-  // getApplicationDocumentsDirectory : 앱을 설치하게 되면, os 에서 앱별로 각각 사용가능한 부분에 지정을 자동으로 해준다. 그것을 가져오는 함수!
-  final dbFolder = await getApplicationDocumentsDirectory();
-  // dbFolder 의 경로에 내가 원하는 이름으로 파일을 저장한다.
-  final file = File(p.join(dbFolder.path, 'db.sqlite'));
+  return LazyDatabase(() async {
+    // getApplicationDocumentsDirectory : 앱을 설치하게 되면, os 에서 앱별로 각각 사용가능한 부분에 지정을 자동으로 해준다. 그것을 가져오는 함수!
+    final dbFolder = await getApplicationDocumentsDirectory();
+    // dbFolder 의 경로에 내가 원하는 이름으로 파일을 저장한다.
+    final file = File(p.join(dbFolder.path, 'db.sqlite'));
 
-  return NativeDatabase(file);
-});
+    return NativeDatabase(file);
+  });
 }

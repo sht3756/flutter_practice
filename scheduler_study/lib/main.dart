e8a40071fbd47e8ee2e921f3bf3e8351b53294fe
SHT-3756 <gkdlfndkekd@naver.com>
2022-12-25 23:57:25 +0900

    feat: main.dart-첫렌더링할때 색상 정보DB에 넣고 불러오기

--- scheduler_study/lib/main.dart
+++ scheduler_study/lib/main.dart
@@ -1,6 +1,26 @@
+import 'package:drift/drift.dart';
 import 'package:flutter/material.dart';
+import 'package:scheduler_study/database/drift_database.dart';
 import 'package:scheduler_study/screen/home_screen.dart';
 import 'package:intl/date_symbol_data_local.dart';
+
+// 색상 배열
+const  DEFAULT_COLORS = [
+  // 빨강
+  'F44336',
+  // 주황
+  'FF9800',
+  // 노랑
+  'FFEB3B',
+  // 초록
+  'FCAF50',
+  // 파랑
+  '2196F3',
+  // 남
+  '3F51B5',
+  // 보라
+  '9C27B0',
+];
 
 void main() async {
   // flutter 프레임 워크가 준비가 된 상태인지 확인하는 함수
@@ -9,6 +29,23 @@ void main() async {
 
   // intl 초기화
   await initializeDateFormatting();
+
+  final database = LocalDatabase();
+
+  // 디비의 카테고리색상 불러오기!
+  final colors = await database.getCategoryColors();
+
+  // 만약 DB 에서 불러온 카테고리 컬러가 없을시 실행
+  if(colors.isEmpty) {
+   for(String hexCode in DEFAULT_COLORS) {
+     await database.createCategoryColor(
+       CategoryColorsCompanion(
+         hexCode: Value(hexCode),
+       )
+     );
+   }
+  }
+
 
   runApp(
     MaterialApp(

ecb193af23f35a4272986f08977cc508a6967639
SHT-3756 <gkdlfndkekd@naver.com>
2022-12-25 23:18:59 +0900

    feat: drift_database.dart-데이터베이스 생성하기

---
+++ scheduler_study/lib/database/drift_database.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+import 'package:drift/drift.dart';
+import 'package:scheduler_study/model/category_color.dart';
+import 'package:scheduler_study/model/schedule.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+import 'package:drift/native.dart';
+
+// part 는 import 보다 조금 더 넓은 기능을 한다.
+// import 는 private 를 불러올 수 없다.
+part 'drift_database.g.dart';
+
+@DriftDatabase(
+  tables: [
+    Schedules,
+    CategoryColors,
+  ],
+)
+
+//  _$LocalDatabase 는 현재 이 클래스에서 선언해놓지 않았다. 하지만 사용가능한 이유는 part 로 불러왔기 떄문이다.
+class LocalDatabase extends _$LocalDatabase {
+  LocalDatabase() : super(_openConnection());
+}
+
+LazyDatabase _openConnection() {
+return LazyDatabase(() async {
+  // getApplicationDocumentsDirectory : 앱을 설치하게 되면, os 에서 앱별로 각각 사용가능한 부분에 지정을 자동으로 해준다. 그것을 가져오는 함수!
+  final dbFolder = await getApplicationDocumentsDirectory();
+  // dbFolder 의 경로에 내가 원하는 이름으로 파일을 저장한다.
+  final file = File(p.join(dbFolder.path, 'db.sqlite'));
+
+  return NativeDatabase(file);
+});
+}

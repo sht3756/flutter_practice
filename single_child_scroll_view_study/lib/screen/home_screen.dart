1454e02cecea25d2e9a3dec0b0e4c790110bb5ab
SHT-3756 <gkdlfndkekd@naver.com>
2022-12-28 18:30:04 +0900

    feat: ListView 공부

--- single_child_scroll_view_study/lib/screen/home_screen.dart
+++ single_child_scroll_view_study/lib/screen/home_screen.dart
@@ -2,8 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:single_child_scroll_view_study/layout/main_layout.dart';
 import 'package:single_child_scroll_view_study/screen/single_child_scroll_view_screen.dart';
 
+import 'list_view_screen.dart';
+
+class ScreenModel {
+  final WidgetBuilder builder;
+  final String name;
+
+  ScreenModel({
+    required this.builder,
+    required this.name,
+  });
+}
+
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  final screens = [
+    ScreenModel(
+      builder: (_) => SingleChildScrollViewScreen(),
+      name: 'SingleChildScrollViewScreen',
+    ),
+    ScreenModel(
+      builder: (_) => ListViewScreen(),
+      name: 'ListViewScreen',
+    ),
+  ];
+
+  HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,15 +37,18 @@ class HomeScreen extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                      builder: (_) => SingleChildScrollViewScreen(),
-                    ));
-                  },
-                  child: Text('SingleChildScrollViewScreen'))
-            ],
+            children: screens
+                .map(
+                  (screen) => ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(builder: screen.builder),
+                      );
+                    },
+                    child: Text(screen.name),
+                  ),
+                )
+                .toList(),
           ),
         ));
   }
---
+++ single_child_scroll_view_study/lib/screen/list_view_screen.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:single_child_scroll_view_study/constant/colors.dart';
+import 'package:single_child_scroll_view_study/layout/main_layout.dart';
+
+class ListViewScreen extends StatelessWidget {
+  final List<int> numbers = List.generate(100, (index) => index);
+
+  ListViewScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MainLayout(
+      title: 'ListViewScreen',
+      body: renderSeparated(),
+    );
+  }
+
+  // 기본: 모두 한번에 그린다.
+  Widget renderDefault() {
+    return ListView(
+      children: numbers
+          .map((e) => renderContainer(
+                color: rainbowColors[e % rainbowColors.length],
+                index: e,
+              ))
+          .toList(),
+    );
+  }
+}
+
+// 2. 보이는것만 그림
+Widget renderBuilder() {
+  return ListView.builder(
+    itemCount: 100,
+    itemBuilder: (context, index) {
+      return renderContainer(
+        color: rainbowColors[index % rainbowColors.length],
+        index: index,
+      );
+    },
+  );
+}
+
+// 3. 2+ 중간 중간마다 추가로 위젯을 넣을 수 있다.
+Widget renderSeparated() {
+  return ListView.separated(
+      itemCount: 100,
+      itemBuilder: (context, index) {
+        return renderContainer(
+            color: rainbowColors[index % rainbowColors.length], index: index);
+      },
+      separatorBuilder: (context, index) {
+        index += 1;
+
+        // 5개의 item마다 배너 보여주기
+        if (index % 5 == 0) {
+          return renderContainer(
+            color: Colors.black,
+            index: index,
+            height: 100,
+          );
+        }
+        return SizedBox(
+          height: 32.0,
+        );
+      });
+}
+
+Widget renderContainer({
+  required Color color,
+  required int index,
+  double? height,
+}) {
+  print(index);
+
+  return Container(
+    height: height ?? 300,
+    color: color,
+    child: Center(
+      child: Text(
+        index.toString(),
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.w700,
+          fontSize: 30.0,
+        ),
+      ),
+    ),
+  );
+}

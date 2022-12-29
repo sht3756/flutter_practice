b72c96170fadcb693cde257a989d76425256902f
SHT-3756 <gkdlfndkekd@naver.com>
2022-12-30 07:48:29 +0900

    feat: HourlyCard 컴포넌트 생성해서 분리 및 import 해주기

---
+++ dusty_study/lib/component/hourly_card.dart
@@ -0,0 +1,58 @@
+import 'package:dusty_study/component/card_title.dart';
+import 'package:dusty_study/component/main_card.dart';
+import 'package:flutter/material.dart';
+
+class HourlyCard extends StatelessWidget {
+  const HourlyCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MainCard(
+        child: Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        CardTitle(
+          title: '시간별 미세먼지',
+        ),
+        Column(
+          children: List.generate(24, (index) {
+            // 현재시간
+            final now = DateTime.now();
+            // 현재 시
+            final hour = now.hour;
+            int currentHour = hour - index;
+
+            // 현재 시 0 보다 작으면 하루를 더한다.
+            if (currentHour < 0) {
+              currentHour += 24;
+            }
+            return Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                      child: Text(
+                    '$currentHour시',
+                  )),
+                  Expanded(
+                    child: Image.asset(
+                      'asset/img/good.png',
+                      height: 20.0,
+                    ),
+                  ),
+                  Expanded(
+                      child: Text(
+                    '좋음',
+                    textAlign: TextAlign.right,
+                  ))
+                ],
+              ),
+            );
+          }),
+        )
+      ],
+    ));
+  }
+}

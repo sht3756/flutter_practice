be13cc3d9c08977a6ccfaeb0d90dd5a3ec0775fc
SHT-3756 <gkdlfndkekd@naver.com>
2023-01-02 02:27:05 +0900

    feat: DioError 인터넷 연결끊겼을시 showSnackBar 출력, RefreshIndicator 추가

--- dusty_study/lib/screen/home_screen.dart
+++ dusty_study/lib/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:dusty_study/container/category_card.dart';
 import 'package:dusty_study/container/hourly_card.dart';
 import 'package:dusty_study/component/main_app_bar.dart';
@@ -40,69 +41,71 @@ class _HomeScreenState extends State<HomeScreen> {
   // 데이터 통신 후 Map 에 넣은 것들을 다 삭제 해준다. 왜냐 Hive 애 넣어서, ValueListenableBuilder 로 변경하기 위해서!
   @override
   Future<void> fetchData() async {
-    final now = DateTime.now();
+    try {
+      final now = DateTime.now();
 
-    // 데이터를 가져와야하는 시간의 기준
-    final fetchTime = DateTime(
-      now.year,
-      now.month,
-      now.day,
-      now.hour
-    );
+      // 데이터를 가져와야하는 시간의 기준
+      final fetchTime = DateTime(now.year, now.month, now.day, now.hour);
 
-    // 아무 box 들고오면 된다.
-    final box = Hive.box<StatModel>(ItemCode.PM10.name);
-    // 가장 최근 데이터
-    final recent = box.values.last as StatModel;
+      // 아무 box 들고오면 된다.
+      final box = Hive.box<StatModel>(ItemCode.PM10.name);
+      // 가장 최근 데이터
+      final recent = box.values.last as StatModel;
 
-    // 데이터를 가져와야하는 시간과 가장 최근 데이터 의 시간이 동일하다면? 밑에있는 작업을 실행할 필요가 없다.
+      // 데이터를 가져와야하는 시간과 가장 최근 데이터 의 시간이 동일하다면? 밑에있는 작업을 실행할 필요가 없다.
 
-    print('fetchTime $fetchTime');
-    print('recent $recent');
-    print(recent.dataTime.isAtSameMomentAs(fetchTime));
-    // isAtSameMomentAs: 같은 순간인지 파악하는 함수 같으면 true, 다르면 false
-    if(recent.dataTime.isAtSameMomentAs(fetchTime)) {
+      // isAtSameMomentAs: 같은 순간인지 파악하는 함수 같으면 true, 다르면 false
+      if (recent.dataTime.isAtSameMomentAs(fetchTime)) {
         print('이미 최신 데이터가 있습니다.');
         return;
-    }
+      }
 
-    List<Future> futures = [];
+      List<Future> futures = [];
 
-    for (ItemCode itemCode in ItemCode.values) {
-      futures.add(
-        StatRepository.fetchData(itemCode: itemCode),
+      for (ItemCode itemCode in ItemCode.values) {
+        futures.add(
+          StatRepository.fetchData(itemCode: itemCode),
+        );
+      }
+
+      final results = await Future.wait(futures);
+
+      // Hive에 데이터 넣기
+      for (int i = 0; i < results.length; i++) {
+        // ItemCode
+        final key = ItemCode.values[i];
+        // List<StatModel>
+        final value = results[i];
+
+        final box = Hive.box<StatModel>(key.name);
+
+        for (StatModel stat in value) {
+          box.put(stat.dataTime.toString(), stat);
+        }
+
+        // 키 값 전부 가져옴
+        final allKeys = box.keys.toList();
+
+        // 24개 이상이면 삭제하는 if 문
+        if (allKeys.length > 24) {
+          // start - 시작 인덱스
+          // end - 끝 인덱스
+
+          // 만약 25개 라면?
+          // allKeys.sublist(0, 25-24)
+          final deleteKeys = allKeys.sublist(0, allKeys.length - 24);
+          // 무조건 24개만 남는다.
+          box.deleteAll(deleteKeys);
+        }
+      }
+    } on DioError catch (e) {
+      // 인터넷이 끊긴다면
+      // final results = await Future.wait(futures); 가 제대로 안되었을때 Dio error 를 잡을 수 있다.
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('인터넷 연결이 원할하지 않습니다.'),
+        ),
       );
-    }
-
-    final results = await Future.wait(futures);
-
-    // Hive에 데이터 넣기
-    for (int i = 0; i < results.length; i++) {
-      // ItemCode
-      final key = ItemCode.values[i];
-      // List<StatModel>
-      final value = results[i];
-
-      final box = Hive.box<StatModel>(key.name);
-
-      for (StatModel stat in value) {
-        box.put(stat.dataTime.toString(), stat);
-      }
-
-      // 키 값 전부 가져옴
-      final allKeys = box.keys.toList();
-
-      // 24개 이상이면 삭제하는 if 문
-      if(allKeys.length > 24) {
-        // start - 시작 인덱스
-        // end - 끝 인덱스
-
-        // 만약 25개 라면?
-        // allKeys.sublist(0, 25-24)
-        final deleteKeys = allKeys.sublist(0, allKeys.length - 24);
-        // 무조건 24개만 남는다.
-        box.deleteAll(deleteKeys);
-      }
     }
   }
 
@@ -146,42 +149,47 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             body: Container(
               color: status.primaryColor,
-              child: CustomScrollView(
-                controller: scrollController,
-                slivers: [
-                  MainAppBar(
-                    stat: recentStat,
-                    status: status,
-                    region: region,
-                    dateTime: recentStat.dataTime,
-                    isExpanded: isExpanded,
-                  ),
-                  SliverToBoxAdapter(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        CategoryCard(
-                          region: region,
-                          darkColor: status.darkColor,
-                          lightColor: status.lightColor,
-                        ),
-                        const SizedBox(height: 16.0),
-                        ...ItemCode.values.map((itemCode) {
-                          return Padding(
-                            padding: const EdgeInsets.only(bottom: 16.0),
-                            child: HourlyCard(
-                              darkColor: status.darkColor,
-                              lightColor: status.lightColor,
-                              region: region,
-                              itemCode: itemCode,
-                            ),
-                          );
-                        }).toList(),
-                        const SizedBox(height: 16.0),
-                      ],
+              child: RefreshIndicator(
+                onRefresh: () async {
+                  await fetchData();
+                },
+                child: CustomScrollView(
+                  controller: scrollController,
+                  slivers: [
+                    MainAppBar(
+                      stat: recentStat,
+                      status: status,
+                      region: region,
+                      dateTime: recentStat.dataTime,
+                      isExpanded: isExpanded,
                     ),
-                  )
-                ],
+                    SliverToBoxAdapter(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          CategoryCard(
+                            region: region,
+                            darkColor: status.darkColor,
+                            lightColor: status.lightColor,
+                          ),
+                          const SizedBox(height: 16.0),
+                          ...ItemCode.values.map((itemCode) {
+                            return Padding(
+                              padding: const EdgeInsets.only(bottom: 16.0),
+                              child: HourlyCard(
+                                darkColor: status.darkColor,
+                                lightColor: status.lightColor,
+                                region: region,
+                                itemCode: itemCode,
+                              ),
+                            );
+                          }).toList(),
+                          const SizedBox(height: 16.0),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           );

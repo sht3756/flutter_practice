7ae38bab732b6a4c64b6e4627aa451cf07b2c712
SHT-3756 <gkdlfndkekd@naver.com>
2023-01-07 18:30:01 +0900

    feat: AppBar와 TabBar 만들기

--- authentication_study/lib/common/layout/default_layout.dart
+++ authentication_study/lib/common/layout/default_layout.dart
@@ -3,18 +3,45 @@ import 'package:flutter/material.dart';
 class DefaultLayout extends StatelessWidget {
   final Widget child;
   final Color? backgroundColor;
+  final String? title;
+  final Widget? bottomNavigationBar;
 
   const DefaultLayout({
     Key? key,
     required this.child,
     this.backgroundColor,
+    this.title,
+    this.bottomNavigationBar,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor ?? Colors.white,
+      appBar: renderAppBar(),
       body: child,
+      bottomNavigationBar: bottomNavigationBar,
     );
+  }
+
+  AppBar? renderAppBar() {
+    // 타이틀이 없을때
+    if (title == null) {
+      return null;
+    } else {
+      // 타이틀이 있을때!
+      return AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          title!,
+          style: TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        foregroundColor: Colors.black,
+      );
+    }
   }
 }
--- authentication_study/lib/common/view/root_tab.dart
+++ authentication_study/lib/common/view/root_tab.dart
@@ -1,11 +1,46 @@
+import 'package:authentication_study/common/const/colors.dart';
 import 'package:authentication_study/common/layout/default_layout.dart';
 import 'package:flutter/material.dart';
 
-class RootTab extends StatelessWidget {
+class RootTab extends StatefulWidget {
   const RootTab({Key? key}) : super(key: key);
 
   @override
+  State<RootTab> createState() => _RootTabState();
+}
+
+class _RootTabState extends State<RootTab> {
+  int index = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return DefaultLayout(child: Column(children: [],));
+    return DefaultLayout(
+      title: '딜리버리',
+      child: Center(
+        child: Text('Root Tab'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: PRIMARY_COLOR,
+        unselectedItemColor: BODY_TEXT_COLOR,
+        selectedFontSize: 10,
+        unselectedFontSize: 10,
+        type: BottomNavigationBarType.fixed,
+        onTap: (int index) {
+          setState(() {
+            this.index = index;
+          });
+        },
+        currentIndex: index,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: '홈'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.fastfood_outlined), label: '음식'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.receipt_long_outlined), label: '주문'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline), label: '프로필')
+        ],
+      ),
+    );
   }
 }

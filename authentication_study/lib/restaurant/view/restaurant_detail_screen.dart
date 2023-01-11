4241569d70d289231d0c88a44464a11e300be79e
SHT-3756 <gkdlfndkekd@naver.com>
2023-01-11 16:13:35 +0900

    feat: SliverList 구현하기

--- authentication_study/lib/restaurant/view/restaurant_detail_screen.dart
+++ authentication_study/lib/restaurant/view/restaurant_detail_screen.dart
@@ -10,24 +10,60 @@ class RestaurantDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultLayout(
       title: '불타는 떡볶이',
-      child: Column(
-        children: [
-          RestaurantCard(
-            image: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
-            name: '불타는 떡볶이',
-            tags: ['떡볶이','계란'],
-            ratingsCount: 100,
-            deliveryTime: 2,
-            deliveryFee: 2000,
-            ratings: 4.0,
-            isDetail: true,
-            detail: '맛있는 떡볶이',
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: ProductCard(),
-          ),
+      child: CustomScrollView(
+        slivers: [
+          renderTop(),
+          renderLabel(),
+          renderProducts(),
         ],
+      ),
+    );
+  }
+
+  SliverToBoxAdapter renderTop() {
+    return SliverToBoxAdapter(
+      child: RestaurantCard(
+        image: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
+        name: '불타는 떡볶이',
+        tags: ['떡볶이', '계란'],
+        ratingsCount: 100,
+        deliveryTime: 2,
+        deliveryFee: 2000,
+        ratings: 4.0,
+        isDetail: true,
+        detail: '맛있는 떡볶이',
+      ),
+    );
+  }
+
+  SliverPadding renderLabel() {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverToBoxAdapter(
+        child: Text(
+          '메뉴',
+          style: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    );
+  }
+
+  renderProducts() {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverList(
+        delegate: SliverChildBuilderDelegate(
+          (context, index) {
+            return Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: ProductCard(),
+            );
+          },
+          childCount: 10,
+        ),
       ),
     );
   }

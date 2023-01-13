5949a35e82f3781b1866a0d0c690eaae0b2c265d
SHT-3756 <gkdlfndkekd@naver.com>
2023-01-13 15:55:04 +0900

    feat: restaurant_screen.dart-Pagination 적용(retrofit 으로 변경해서 모델링한 로직 다 제거, 공통으로 분리해 만든 페이징처리 적용)

--- authentication_study/lib/restaurant/view/restaurant_screen.dart
+++ authentication_study/lib/restaurant/view/restaurant_screen.dart
@@ -2,6 +2,7 @@ import 'package:authentication_study/common/const/data.dart';
 import 'package:authentication_study/restaurant/component/restaurant_card.dart';
 import 'package:authentication_study/restaurant/dio/dio.dart';
 import 'package:authentication_study/restaurant/model/restaurant_model.dart';
+import 'package:authentication_study/restaurant/repository/restaurant_repository.dart';
 import 'package:authentication_study/restaurant/view/restaurant_detail_screen.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
@@ -9,21 +10,16 @@ import 'package:flutter/material.dart';
 class RestaurantScreen extends StatelessWidget {
   const RestaurantScreen({Key? key}) : super(key: key);
 
-  Future<List> paginateRestaurant() async {
+  Future<List<RestaurantModel>> paginateRestaurant() async {
     final dio = Dio();
 
     dio.interceptors.add(CustomInterceptor(storage: storage));
 
-    final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
+    final res =
+        await RestaurantRepository(dio, baseUrl: 'http://$ip/restaurant')
+            .paginate();
 
-    final res = await dio.get(
-      'http://$ip/restaurant',
-      options: Options(headers: {
-        'authorization': 'Bearer $accessToken',
-      }),
-    );
-
-    return res.data['data'];
+    return res.data;
   }
 
   @override
@@ -32,16 +28,15 @@ class RestaurantScreen extends StatelessWidget {
       child: Center(
           child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: FutureBuilder<List>(
+        child: FutureBuilder<List<RestaurantModel>>(
           future: paginateRestaurant(),
-          builder: (context, AsyncSnapshot<List> snapshot) {
+          builder: (context, AsyncSnapshot<List<RestaurantModel>> snapshot) {
             if (!snapshot.hasData) {
               return Center(child: CircularProgressIndicator());
             }
             return ListView.separated(
                 itemBuilder: (_, index) {
-                  final item = snapshot.data![index];
-                  final parseItem = RestaurantModel.fromJson(item);
+                  final parseItem = snapshot.data![index];
 
                   // RestaurantModel 이 매핑이 되어 들어오면 RestaurantCard 로 자동으로 매핑되게 설정
                   return GestureDetector(

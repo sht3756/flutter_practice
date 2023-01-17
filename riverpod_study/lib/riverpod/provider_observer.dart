f3d41fcb7ffd080463c417f291685d98202304b3
SHT-3756 <gkdlfndkekd@naver.com>
2023-01-17 23:40:00 +0900

    feat: ProviderObserver 적용(didUpdateProvider, didAddProvider, didDisposeProvider 일때 로깅)

---
+++ riverpod_study/lib/riverpod/provider_observer.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class Logger extends ProviderObserver {
+  // Provider 가 업데이트 되었을때
+  @override
+  void didUpdateProvider(ProviderBase provider, Object? previousValue,
+      Object? newValue, ProviderContainer container) {
+    print(
+      '[Provider Updated] provider: $provider / pv: $previousValue / nv: $newValue',
+    );
+  }
+
+  // Provider 가 추가 되었을때
+  @override
+  void didAddProvider(
+      ProviderBase provider, Object? value, ProviderContainer container) {
+    print(
+      '[Provider Added] provider: $provider / value: $value',
+    );
+  }
+
+  // Provider 가 종료 되었을때
+  @override
+  void didDisposeProvider(ProviderBase provider, ProviderContainer container) {
+    print(
+      '[Provider Added] provider: $provider',
+    );
+  }
+}

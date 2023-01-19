81f264f97e351079fd11cf87b67b1143ca23fcbb
SHT-3756 <gkdlfndkekd@naver.com>
2023-01-19 20:54:19 +0900

    feat: RestaurantProvider 작업(StateNotifierProvider 를 통해 paginate 함수 호출하여 결과적으로 스크린에서 FutureBuilder 함수 삭제)

---
+++ authentication_study/lib/restaurant/provider/restaurant_provider.dart
@@ -0,0 +1,34 @@
+import 'package:authentication_study/restaurant/model/restaurant_model.dart';
+import 'package:authentication_study/restaurant/repository/restaurant_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// RestaurantStateNotifier 를 리턴해주는 provider 만들었다.
+final restaurantProvider =
+    StateNotifierProvider<RestaurantStateNotifier, List<RestaurantModel>>(
+        (ref) {
+  // provider 안의 provider 를 가져오는 방법!
+  final repository = ref.watch(restaurantRepositoryProvider);
+  final notifier = RestaurantStateNotifier(repository: repository);
+
+  return notifier;
+});
+
+class RestaurantStateNotifier extends StateNotifier<List<RestaurantModel>> {
+  final RestaurantRepository repository;
+
+  // 페이지네이션 기능과 디테일페이지를 가져오는 기능을 Provider 안에서 수행하고 data 를 받아오고 super constructor 의 list 에 넣어줄것이다.
+  RestaurantStateNotifier({
+    required this.repository,
+  }) : super([]) {
+    paginate();
+  }
+
+  // repository 를 정상적으로 받았다는 가정하에 paginate 함수 안에서 페이지네이션을 진행하고 값을 반환받는 것이 아니라
+  // 상태 안에다 응답받은 리스트로 된 RestaurantModel 을 집어넣을것이다.
+  // 위젯에서는 상태를 바라보고있다가 상태가 변화하면 화면에 새로운값으로 랜더링 해줄것이다.
+  paginate() async {
+    final res = await repository.paginate();
+
+    state = res.data;
+  }
+}
--- authentication_study/lib/restaurant/view/restaurant_screen.dart
+++ authentication_study/lib/restaurant/view/restaurant_screen.dart
@@ -1,7 +1,5 @@
-import 'package:authentication_study/common/model/cursor_pagination_model.dart';
 import 'package:authentication_study/restaurant/component/restaurant_card.dart';
-import 'package:authentication_study/restaurant/model/restaurant_model.dart';
-import 'package:authentication_study/restaurant/repository/restaurant_repository.dart';
+import 'package:authentication_study/restaurant/provider/restaurant_provider.dart';
 import 'package:authentication_study/restaurant/view/restaurant_detail_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -11,39 +9,36 @@ class RestaurantScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container(
-      child: Center(
-          child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: FutureBuilder<CursorPagination<RestaurantModel>>(
-          future: ref.watch(restaurantRepositoryProvider).paginate(),
-          builder: (context, AsyncSnapshot<CursorPagination<RestaurantModel>> snapshot) {
-            if (!snapshot.hasData) {
-              return Center(child: CircularProgressIndicator());
-            }
-            return ListView.separated(
-                itemBuilder: (_, index) {
-                  final parseItem = snapshot.data!.data[index];
+    final data = ref.watch(restaurantProvider);
 
-                  // RestaurantModel 이 매핑이 되어 들어오면 RestaurantCard 로 자동으로 매핑되게 설정
-                  return GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                        builder: (_) => RestaurantDetailScreen(
-                          id: parseItem.id,
-                        ),
-                      ));
-                    },
-                    child: RestaurantCard.fromModel(model: parseItem),
-                  );
-                },
-                separatorBuilder: (_, index) {
-                  return SizedBox(height: 16.0);
-                },
-                itemCount: snapshot.data!.data.length);
-          },
-        ),
-      )),
+    // TODO : 임시로 분기처리 작성
+    if (data.length == 0) {
+      return Center(child: CircularProgressIndicator());
+    }
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: ListView.separated(
+        itemCount: data.length,
+        itemBuilder: (_, index) {
+          final parseItem = data[index];
+
+          // RestaurantModel 이 매핑이 되어 들어오면 RestaurantCard 로 자동으로 매핑되게 설정
+          return GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (_) => RestaurantDetailScreen(
+                  id: parseItem.id,
+                ),
+              ));
+            },
+            child: RestaurantCard.fromModel(model: parseItem),
+          );
+        },
+        separatorBuilder: (_, index) {
+          return SizedBox(height: 16.0);
+        },
+      ),
     );
   }
 }

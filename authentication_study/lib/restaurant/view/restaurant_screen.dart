6182b5cff4dea8ebb20b78c0ba05c3d75941fbe1
SHT-3756 <gkdlfndkekd@naver.com>
2023-01-21 01:59:21 +0900

    feat: Pagination로직 실행해보기(현재스크롤 위치값, 최대 길이값 구해 새로운 데이터 요청)

--- authentication_study/lib/restaurant/view/restaurant_screen.dart
+++ authentication_study/lib/restaurant/view/restaurant_screen.dart
@@ -5,19 +5,55 @@ import 'package:authentication_study/restaurant/view/restaurant_detail_screen.da
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class RestaurantScreen extends ConsumerWidget {
+class RestaurantScreen extends ConsumerStatefulWidget {
   const RestaurantScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<RestaurantScreen> createState() => _RestaurantScreenState();
+}
+
+class _RestaurantScreenState extends ConsumerState<RestaurantScreen> {
+  // 현재 위치를 알기 위한 스크롤 컨트롤러
+  final ScrollController controller = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller.addListener(scrollListener);
+  }
+
+  void scrollListener() {
+    // 현재 스클롤 위치가 최대 길이 만큼 거의 도달했을떄 새로운데이터 요청
+    // 최대 길이 -300 보다 현재 스크롤 위치가 크면 실행
+    if (controller.offset > controller.position.maxScrollExtent - 300) {
+      ref.read(restaurantProvider.notifier).paginate(
+            fetchMore: true,
+          );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final data = ref.watch(restaurantProvider);
 
-    // 타입비교
+    // 완전 처음 랜더링할때
     if (data is CursorPaginationLoading) {
       return Center(
         child: CircularProgressIndicator(),
       );
     }
+
+    // 에러날 때
+    if (data is CursorPaginationError) {
+      return Center(
+        child: Text(data.message),
+      );
+    }
+
+    // CursorPagination
+    // CursorPaginationFetchingMore
+    // CursorPaginationRefetching
 
     // 캐스팅
     final cp = data as CursorPagination;
@@ -25,6 +61,7 @@ class RestaurantScreen extends ConsumerWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: ListView.separated(
+        controller: controller,
         itemCount: cp.data.length,
         itemBuilder: (_, index) {
           final parseItem = cp.data[index];

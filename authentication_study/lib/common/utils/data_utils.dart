50762a41f16ce7cc6fff0d59a8990194b17a4807
SHT-3756 <gkdlfndkekd@naver.com>
2023-01-22 18:38:35 +0900

    feat:Rating Model 작업하기, 이미지 list 로 받는 static 함수 생성

--- authentication_study/lib/common/utils/data_utils.dart
+++ authentication_study/lib/common/utils/data_utils.dart
@@ -1,8 +1,14 @@
 import 'package:authentication_study/common/const/data.dart';
 
 class DataUtils {
+  // string 형태로 이미지 한개 받기
   static pathToUrl(String value) {
     // value 는 thumbUrl 이다.
     return 'http://$ip$value';
+  }
+
+  // list 형태로 이미지 여러개 받기
+  static listPathsToUrls(List<String> paths){
+    return paths.map((e)=> pathToUrl(e)).toList();
   }
 }

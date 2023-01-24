f9e95792b1d71aa795cd370874b2987275c942e2
SHT-3756 <gkdlfndkekd@naver.com>
2023-01-25 00:59:31 +0900

    feat: pop 함수를 이용해서 프로그램적으로 뒤로가기

--- go_router_study/lib/screen/1_screen.dart
+++ go_router_study/lib/screen/1_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:go_router_study/layout/default_layout.dart';
 
 class OneScreen extends StatelessWidget {
@@ -8,7 +9,16 @@ class OneScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultLayout(
         body: Column(
-      children: [],
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            context.pop();
+          },
+          child: Text('pop'),
+        ),
+      ],
     ));
   }
 }

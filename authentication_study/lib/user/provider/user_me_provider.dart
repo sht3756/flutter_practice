cf5e0a51c9c4b6e663cfa4dd1cb0d2ade3d6bb63
SHT-3756 <gkdlfndkekd@naver.com>
2023-01-27 13:28:00 +0900

    feat: GetMe 함수안 로직 추가(로그아웃 되었다는 로직)

--- authentication_study/lib/user/provider/user_me_provider.dart
+++ authentication_study/lib/user/provider/user_me_provider.dart
@@ -28,6 +28,8 @@ class UserMeStateNotifier extends StateNotifier<UserModelBase?> {
 
     // refresh , accessToken 둘중 하나라도 없으면, 함수 실행 못하게 하는 조건
     if (refreshToken == null || accessToken == null) {
+      // 로그아웃 된 상태라고 알려주는 null
+      state = null;
       return;
     }
 
@@ -38,11 +40,10 @@ class UserMeStateNotifier extends StateNotifier<UserModelBase?> {
 }
 
 /*
- 순서! 현재 사용자를 가져오는 로직
+ getMe 현재 사용자를 가져오는 로직
  1. 어떤 레포지토리에서 가져올지 인스턴스에 required 선언한다.
  2. 토큰을 가져올 스토리지를 인스턴스에 required 선언한다.
  3. 토큰에서 accessToken 과 refreshToken 을 가져온다.
-
-
-
+ 4. 만약 토큰 둘중 하나라도 없으면 상태에 로그아웃 상태로 넣어준다.
+ 5. 토큰이 둘다 있으면 api 통신해서 응답받은 값(현재 로그인된 유저의 토큰 id, 이름, 이미지 url) 를 상태에 넣어준다.
 */

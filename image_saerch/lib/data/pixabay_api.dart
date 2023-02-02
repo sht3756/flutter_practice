7632fdf52d4253c9a8fce61334d510bb0d454d29
SHT-3756 <gkdlfndkekd@naver.com>
2023-02-03 02:34:17 +0900

    feat: Mockito 를 활용한 테스트

--- image_saerch/lib/data/pixabay_api.dart
+++ image_saerch/lib/data/pixabay_api.dart
@@ -5,11 +5,16 @@ import 'package:image_saerch/model/photo_model.dart';
 import 'package:http/http.dart' as http;
 
 class PixabayApi implements PhotoApiRepository{
-  final baseUrl = 'https://pixabay.com/api/';
-  final key = '33231274-294c2e9b82f250dc8b6ae5e26';
+  static const baseUrl = 'https://pixabay.com/api/';
+  static const key = '33231274-294c2e9b82f250dc8b6ae5e26';
 
-  Future<List<PhotoModel>> fetch(String query) async {
-    final res = await http.post(Uri.parse(
+  @override
+  Future<List<PhotoModel>> fetch(String query, {http.Client? client}) async {
+    // client 가 null 일때 http.Client() 로 초기화하겠다.
+    client ??= http.Client();
+
+    // http 기본 client 사용 하고 client.get 을 하면 지정한 client 있으면 사용하고 아니면 기본 client 로
+    final res = await client.get(Uri.parse(
         '$baseUrl?key=$key&q=$query&image_type=photo&pretty=true'));
 
     Map<String, dynamic> jsonResponse = jsonDecode(res.body);

ebd908005faa321aad929a568e838a6d836dcb80
SHT-3756 <gkdlfndkekd@naver.com>
2023-02-06 18:06:19 +0900

    feat: DI(의존성 주입)

---
+++ image_saerch/lib/di/provider_setup.dart
@@ -0,0 +1,42 @@
+import 'package:image_saerch/data/data_source/pixabay_api.dart';
+import 'package:image_saerch/data/repository/photo_api_repository_impl.dart';
+import 'package:image_saerch/domain/repository/photo_api_repository.dart';
+import 'package:image_saerch/domain/use_case/get_photos_use_case.dart';
+import 'package:image_saerch/presentation/home/home_view_model.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
+import 'package:http/http.dart' as http;
+
+// 1. provider 전체
+List<SingleChildWidget> globalProviders = [
+  ...independentModels,
+  ...dependentModels,
+  ...viewModels,
+];
+
+// 2. 독립적인 객체
+List<SingleChildWidget> independentModels = [
+  Provider<http.Client>(
+    create: (context) => http.Client(),
+  ),
+];
+
+// 3. 2번의 의존성이 있는 객체
+List<SingleChildWidget> dependentModels = [
+  ProxyProvider<http.Client, PixabayApi>(
+    update: (context, client, _) => PixabayApi(client),
+  ),
+  ProxyProvider<PixabayApi, PhotoApiRepository>(
+    update: (context, api, _) => PhotoApiRepositoryImpl(api),
+  ),
+  ProxyProvider<PhotoApiRepository, GetPhotosUseCase>(
+    update: (context, repository, _) => GetPhotosUseCase(repository),
+  ),
+];
+
+// 4. ViewModels
+List<SingleChildWidget> viewModels = [
+  ChangeNotifierProvider<HomeViewModel>(
+    create: (context) => HomeViewModel(context.read<GetPhotosUseCase>()),
+  ),
+];
--- image_saerch/lib/main.dart
+++ image_saerch/lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:image_saerch/data/data_source/pixabay_api.dart';
-import 'package:image_saerch/data/repository/photo_api_repository_impl.dart';
-import 'package:image_saerch/domain/use_case/get_photos_use_case.dart';
+import 'package:image_saerch/di/provider_setup.dart';
 import 'package:image_saerch/presentation/home/home_screen.dart';
-import 'package:image_saerch/presentation/home/home_view_model.dart';
 import 'package:provider/provider.dart';
-import 'package:http/http.dart' as http;
+
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: globalProviders,
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -20,10 +22,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ChangeNotifierProvider(
-        create: (_) => HomeViewModel(GetPhotosUseCase(PhotoApiRepositoryImpl(PixabayApi(http.Client())))),
-        child: const HomeScreen(),
-      ),
+      home: const HomeScreen(),
     );
   }
 }

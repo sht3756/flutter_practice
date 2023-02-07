ffb46ceea294924c1f55931b49a23f19f0c4ef1d
SHT-3756 <gkdlfndkekd@naver.com>
2023-02-07 21:27:17 +0900

    feat: 첫번째 화면 UI 작성

---
+++ note_app/lib/presentation/notes/note_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/domain/model/note.dart';
+import 'package:note_app/presentation/notes/components/note_item.dart';
+import 'package:note_app/ui/colors.dart';
+
+class NoteScreen extends StatelessWidget {
+  const NoteScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Your note',
+          style: TextStyle(fontSize: 30.0),
+        ),
+        centerTitle: false,
+        elevation: 0,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.sort),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+      body: ListView(
+        children: [
+          NoteItem(
+            note: Note(
+              title: 'title1',
+              content: 'content',
+              color: wisteria.value,
+              timestamp: 1,
+            ),
+          ),
+          NoteItem(
+            note: Note(
+              title: 'title2',
+              content: 'content',
+              color: skyBlue.value,
+              timestamp: 1,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

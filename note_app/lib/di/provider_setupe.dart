e02f5ce665d2e2886a47f323ef3a79b85352d727
SHT-3756 <gkdlfndkekd@naver.com>
2023-02-08 14:55:30 +0900

    feat: DI 설정 해서 DI 주입

---
+++ note_app/lib/di/provider_setupe.dart
@@ -0,0 +1,29 @@
+import 'package:note_app/data/data_source/note_db_helper.dart';
+import 'package:note_app/data/repository/note_repository_impl.dart';
+import 'package:note_app/domain/repository/note_repository.dart';
+import 'package:note_app/presentation/add_edit_note/add_edit_note_view_model.dart';
+import 'package:note_app/presentation/notes/notes_view_model.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
+import 'package:sqflite/sqflite.dart';
+
+Future<List<SingleChildWidget>> getProviders() async {
+  Database database =
+      await openDatabase('note_db', version: 1, onCreate: (db, version) async {
+    // DB 생성
+    await db.execute(
+      'CREATE TABLE note (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, content TEXT, color INTEGER, timestamp INTEGER)',
+    );
+  });
+
+  // 의존성 주입
+  NoteDbHelper noteDbHelper = NoteDbHelper(database);
+  NoteRepository repository = NoteRepositoryImpl(noteDbHelper);
+  NotesViewModel notesViewModel = NotesViewModel(repository);
+  AddEditNoteViewModel addEditNoteViewModel = AddEditNoteViewModel(repository);
+
+  return [
+    ChangeNotifierProvider(create: (_) => notesViewModel),
+    ChangeNotifierProvider(create: (_) => addEditNoteViewModel),
+  ];
+}
--- note_app/lib/main.dart
+++ note_app/lib/main.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/di/provider_setupe.dart';
 import 'package:note_app/presentation/notes/note_screen.dart';
 import 'package:note_app/ui/colors.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  // 플랫폼 채널의 위젯 바인딩을 보장한다.(getProviders 에서 디비를 연결할때 native 코드를 사용할텐데, 그 코드가 연결이 다 된후 실행하기 위함.)
+  WidgetsFlutterBinding.ensureInitialized();
+  // 프로바이더 가져와서
+  final providers = await getProviders();
+
+  // MultiProvider 로 한번에 넣어주기!
+  runApp(
+    MultiProvider(
+        providers: providers,
+        child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -15,16 +28,22 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primaryColor: Colors.white,
-        backgroundColor: darkGray,
-        canvasColor: darkGray,
-        floatingActionButtonTheme: Theme.of(context).floatingActionButtonTheme.copyWith(
-          backgroundColor: Colors.white,
-          foregroundColor: darkGray,
-        ),
-        appBarTheme: Theme.of(context).appBarTheme.copyWith(
+          primaryColor: Colors.white,
           backgroundColor: darkGray,
-        )
+          canvasColor: darkGray,
+          floatingActionButtonTheme: Theme
+              .of(context)
+              .floatingActionButtonTheme
+              .copyWith(
+            backgroundColor: Colors.white,
+            foregroundColor: darkGray,
+          ),
+          appBarTheme: Theme
+              .of(context)
+              .appBarTheme
+              .copyWith(
+            backgroundColor: darkGray,
+          )
       ),
       home: const NoteScreen(),
     );

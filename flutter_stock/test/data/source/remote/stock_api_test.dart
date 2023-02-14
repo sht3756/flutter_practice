c5a0632e42ed052da59fdbb87184f100676c2454
SHT-3756 <gkdlfndkekd@naver.com>
2023-02-15 03:16:28 +0900

    feat: StockApi 테스트 코드 작성

---
+++ flutter_stock/test/data/source/remote/stock_api_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_stock/data/csv/company_listing_parser.dart';
+import 'package:flutter_stock/data/source/remote/stock_api.dart';
+import 'package:flutter_test/flutter_test.dart';
+void main() {
+  test('네트워크 통신', () async {
+    final response = await StockApi().getListings();
+
+    final _parser = CompanyListingsParser();
+    final remoteListings = await _parser.parse(response.body);
+
+    expect(remoteListings[0].symbol, 'A');
+    expect(remoteListings[0].name, 'Agilent Technologies Inc');
+    expect(remoteListings[0].exchange, 'NYSE');
+  });
+}

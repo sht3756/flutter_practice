2cfaf82707a59986ab4604911ce852a27e092b40
SHT-3756 <gkdlfndkekd@naver.com>
2023-02-15 03:12:16 +0900

    feat: CSV 파싱, 캐시구현

--- flutter_stock/lib/data/repository/stock_repository_impl.dart
+++ flutter_stock/lib/data/repository/stock_repository_impl.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_stock/data/csv/company_listing_parser.dart';
 import 'package:flutter_stock/data/mapper/company_mapper.dart';
 import 'package:flutter_stock/data/source/local/stock_dao.dart';
 import 'package:flutter_stock/data/source/remote/stock_api.dart';
@@ -8,31 +9,33 @@ import 'package:flutter_stock/util/result.dart';
 class StockRepositoryImpl implements StockRepository {
   final StockApi _api;
   final StockDao _dao;
+  final _parser = CompanyListingsParser();
 
   StockRepositoryImpl(this._api, this._dao);
 
   @override
   Future<Result<List<CompanyListing>>> getCompanyListings(
       {required bool fetchFromRemote, required String query}) async {
-    // 캐시에서 찾기
     final localListings = await _dao.searchCompanyListing(query);
 
-    // 없으면 리모트에서 가져오기
     final isDbEmpty = localListings.isEmpty && query.isEmpty;
     final shouldJustLoadFromCache = !isDbEmpty && !fetchFromRemote;
 
-    // 캐시
     if (shouldJustLoadFromCache) {
       // 확장 메소드 사용 toCompanyListing()
       return Result.success(
           localListings.map((e) => e.toCompanyListing()).toList());
     }
 
-    // 리모트
     try {
-      final remoteListings = await _api.getListings();
-      // TODO: CSV 파싱 변환
-      return const Result.success([]);
+      final res = await _api.getListings();
+      final remoteListings = await _parser.parse(res.body);
+
+      await _dao.clearCompanyListings();
+      await _dao.insertCompanyListings(
+          remoteListings.map((e) => e.toCompanyListingEntity()).toList());
+
+      return Result.success(remoteListings);
     } catch (e) {
       return Result.error(Exception('데이터 로드 실패!'));
     }
--- flutter_stock/lib/data/source/remote/stock_api.dart
+++ flutter_stock/lib/data/source/remote/stock_api.dart
@@ -4,12 +4,12 @@ class StockApi {
   static const baseUrl = 'https://www.alphavantage.co/';
   static const apiKey = '0BC3F8Y6HH3J8OPZ';
 
-  final http.Client client;
+  final http.Client _client;
 
-  StockApi({required this.client});
+  StockApi({http.Client? client}) : _client = (client ?? http.Client());
 
   Future<http.Response> getListings({String apiKey = apiKey}) async {
-    return await client.get(Uri.parse(
+    return await _client.get(Uri.parse(
         'https://www.alphavantage.co/query?function=LISTING_STATUS&apikey=$apiKey'));
   }
 }

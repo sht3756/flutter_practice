bb0f2b29a21ea44c9e6c52fa269926954f561517
SHT-3756 <gkdlfndkekd@naver.com>
2023-02-15 01:01:16 +0900

    feat: Material3 적용

--- flutter_stock/lib/main.dart
+++ flutter_stock/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_stock/color_schemes.g.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,16 +14,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        useMaterial3: true,
+        colorScheme: lightColorScheme,
+      ),
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        colorScheme: darkColorScheme,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );

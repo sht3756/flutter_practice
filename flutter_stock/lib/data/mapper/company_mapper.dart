98aeea7065fa9db56995b721080fe1f081cf6f03
SHT-3756 <gkdlfndkekd@naver.com>
2023-02-15 02:25:14 +0900

    feat: mapper 작성

---
+++ flutter_stock/lib/data/mapper/company_mapper.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_stock/data/source/local/company_listing_entity.dart';
+import 'package:flutter_stock/domain/model/company_listing.dart';
+
+extension ToCompanyListing on CompanyListingEntity {
+  CompanyListing toCompanyListing() {
+    return CompanyListing(
+      symbol: symbol,
+      name: name,
+      exchange: exchange,
+    );
+  }
+}
+
+extension ToCompanyListingEntity on CompanyListing {
+  CompanyListingEntity toCompanyListingEntity() {
+    return CompanyListingEntity(
+      symbol: symbol,
+      name: name,
+      exchange: exchange,
+    );
+  }
+}

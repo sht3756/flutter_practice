b08acb486633ff438ca558c36af603a5b54294c6
SHT-3756 <gkdlfndkekd@naver.com>
2023-02-16 17:32:09 +0900

    feat: viewModel viewScreen 에 적용

--- flutter_stock/lib/presentation/company_listings/company_listings_screen.dart
+++ flutter_stock/lib/presentation/company_listings/company_listings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_stock/presentation/company_listings/company_listings_action.dart';
 import 'package:flutter_stock/presentation/company_listings/company_listings_view_model.dart';
 import 'package:provider/provider.dart';
 
@@ -16,6 +17,11 @@ class CompanyListingsScreen extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: TextField(
+                onChanged: (query) {
+                  viewModel.onAction(
+                    CompanyListingsAction.onSearchQueryChange(query)
+                  );
+                },
                 decoration: InputDecoration(
                   focusedBorder: OutlineInputBorder(
                     borderSide: BorderSide(
@@ -39,7 +45,9 @@ class CompanyListingsScreen extends StatelessWidget {
             ),
             Expanded(
               child: RefreshIndicator(
-                onRefresh: () async {},
+                onRefresh: () async {
+                  viewModel.onAction(const CompanyListingsAction.refresh());
+                },
                 child: ListView.builder(
                   itemCount: state.companies.length,
                   itemBuilder: (context, index) {

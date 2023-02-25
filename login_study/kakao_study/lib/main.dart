f1e81bfb3e8b9b8d9cd9658562de67f54128bfc1
SHT-3756 <gkdlfndkekd@naver.com>
2023-02-26 01:50:02 +0900

    feat: 카카오톡 로그인 테스트

--- login_study/kakao_study/lib/main.dart
+++ login_study/kakao_study/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
+import 'package:kakao_study/screen/sample_screen.dart';
 
 void main() {
   KakaoSdk.init(nativeAppKey: '75fcc4947e21cd40196302638d985b8b');
@@ -9,13 +11,87 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // @override
+  // Widget build(BuildContext context) {
+  //
+  //   return MaterialApp(
+  //     debugShowCheckedModeBanner: false,
+  //     home: const SampleScreen(),
+  //   );
+  // }
+
+
+  text() async {
+    // 카카오 로그인 구현 예제
+
+    // 카카오톡 설치 여부 확인
+    // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+    if (await isKakaoTalkInstalled()) {
+      try {
+        await UserApi.instance.loginWithKakaoTalk();
+        print('카카오톡으로 로그인 성공');
+      } catch (error) {
+        print('카카오톡으로 로그인 실패 $error');
+
+        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+        if (error is PlatformException && error.code == 'CANCELED') {
+          return;
+        }
+        // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
+        try {
+          await UserApi.instance.loginWithKakaoAccount();
+          print('카카오계정으로 로그인 성공');
+        } catch (error) {
+          print('카카오계정으로 로그인 실패 $error');
+        }
+      }
+    } else {
+      try {
+        await UserApi.instance.loginWithKakaoAccount();
+        print('카카오계정으로 로그인 성공');
+      } catch (error) {
+        print('카카오계정으로 로그인 실패 $error');
+      }
+    }
+
+    // 사용자 정보 가져오기
+    try {
+      User user = await UserApi.instance.me();
+      print('사용자 정보 요청 성공'
+          '\n회원번호: ${user.id}'
+          '\n닉네임: ${user.kakaoAccount?.profile?.nickname}'
+          '\n이메일: ${user.kakaoAccount?.email}'
+          '\n전부: ${user}'
+      );
+    } catch (error) {
+      print('사용자 정보 요청 실패 $error');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: const Center(
-        child: Text('Flutter Demo Home Page'),
-      ),
-    );
+        title: 'Flutter Demo',
+        home: Scaffold(
+          body: SafeArea(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('안녕'),
+                  TextButton(
+                    onPressed: () {
+                      text();
+                    },
+                    child: Text(
+                      '카카로로그인',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }

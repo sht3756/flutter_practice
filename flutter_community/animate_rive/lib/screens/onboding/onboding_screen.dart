d9a62706ef133b478159087443f9d3d1e63bc0d7
SHT-3756 <gkdlfndkekd@naver.com>
2023-02-26 02:33:52 +0900

    feat: UI 추가 및 버튼 Rive-animation 액션 추가

---
+++ flutter_community/animate_rive/lib/screens/onboding/onboding_screen.dart
@@ -0,0 +1,132 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  late RiveAnimationController _btnAnimationController;
+
+  @override
+  void initState() {
+    // 애니메이션 추가
+    _btnAnimationController = OneShotAnimation(
+      "active",
+      autoplay: false,
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            width: MediaQuery.of(context).size.width * 1.7,
+            bottom: 200,
+            left: 100,
+            child: Image.asset(
+              "assets/Backgrounds/Spline.png",
+            ),
+          ),
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 20, sigmaY: 10),
+              child: SizedBox(),
+            ),
+          ),
+          // 이미지 불러오기
+          const RiveAnimation.asset(
+            "assets/RiveAssets/shapes.riv",
+          ),
+          // 블러추가
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 20, sigmaY: 30), //수평 수직방향 흐림효과
+              child: const SizedBox(),
+            ),
+          ),
+
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Spacer(), // Spacer 는 임의의 공간을 준다.
+                  SizedBox(
+                    width: 260,
+                    child: Column(
+                      children: const [
+                        Text(
+                          'Learn design  & code',
+                          style: TextStyle(
+                            fontSize: 60,
+                            fontFamily: "Poppins",
+                            height: 1.2, // 글자간 높이
+                          ),
+                        ),
+                        SizedBox(height: 16),
+                        Text(
+                          "Don’t skip design. Learn design and code, by building real apps with Flutter and Swift. Complete courses about the best tools.",
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Spacer(flex: 2),
+                  GestureDetector(
+                    onTap: () {
+                      // 애니메이션 시작
+                      _btnAnimationController.isActive = true;
+                    },
+                    child: SizedBox(
+                      height: 64,
+                      width: 260,
+                      child: Stack(
+                        children: [
+                           RiveAnimation.asset(
+                              "assets/RiveAssets/button.riv",
+                            // 컨트롤 추가
+                            controllers: [_btnAnimationController],
+                          ),
+                          Positioned.fill(
+                              top: 8,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: const [
+                                  Icon(CupertinoIcons.arrow_right),
+                                  SizedBox(width: 8),
+                                  Text(
+                                    'Start the course',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  )
+                                ],
+                              ))
+                        ],
+                      ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 24),
+                    child: Text(
+                        "Purchase includes access to 30+ courses, 240+ premium tutorials, 120+ hours of videos, source files and certificates."),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

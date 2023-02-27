4dd4c17b2823d503e8561526d37e33faed3748a2
SHT-3756 <gkdlfndkekd@naver.com>
2023-02-27 16:20:13 +0900

    feat: 사이드바 추가

--- flutter_community/animate_rive/lib/screens/onboding/components/entry_point.dart
+++ flutter_community/animate_rive/lib/screens/onboding/components/entry_point.dart
@@ -1,4 +1,8 @@
+import 'dart:math';
+
+import 'package:animate_rive/components/side_menu.dart';
 import 'package:animate_rive/constants.dart';
+import 'package:animate_rive/models/menu_btn.dart';
 import 'package:animate_rive/models/rive_asset.dart';
 import 'package:animate_rive/screens/home/home_screen.dart';
 import 'package:animate_rive/utils/rive_utils.dart';
@@ -14,69 +18,161 @@ class EntryPoint extends StatefulWidget {
   State<EntryPoint> createState() => _EntryPointState();
 }
 
-class _EntryPointState extends State<EntryPoint> {
+class _EntryPointState extends State<EntryPoint> with SingleTickerProviderStateMixin{
   RiveAsset selectedBottomNav = bottomNavs.first;
+  late AnimationController _animationController;
+  late Animation<double> animation;
+  late Animation<double> scaleAnimation;
 
+  late SMIBool isSideBarClosed;
+
+  bool isSideMenuClosed = true;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 200),
+    )..addListener(() {
+      setState(() {});
+    });
+
+    animation = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+          parent: _animationController, curve: Curves.fastOutSlowIn),
+    );
+    scaleAnimation = Tween<double>(begin: 1, end: 0.8).animate(
+      CurvedAnimation(
+          parent: _animationController, curve: Curves.fastOutSlowIn),
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: HomeScreen(),
-      bottomNavigationBar: SafeArea(
-        child: Container(
-          padding: const EdgeInsets.all(12),
-          margin: const EdgeInsets.symmetric(horizontal: 24),
-          decoration: BoxDecoration(
-            color: backgroundColor2.withOpacity(0.8),
-            borderRadius: BorderRadius.all(Radius.circular(24)),
+      backgroundColor: backgroundColor2,
+      resizeToAvoidBottomInset: false,
+      extendBody: true,
+      body: Stack(
+        children: [
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 200),
+            curve: Curves.fastOutSlowIn,
+            width: 288,
+            left: isSideMenuClosed ? -288 : 0,
+            height: MediaQuery.of(context).size.height,
+            child: const SideMenu(),
           ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              ...List.generate(
-                  bottomNavs.length,
-                  (index) => GestureDetector(
-                        onTap: () {
-                          bottomNavs[index].input!.change(true);
-                          if (bottomNavs[index] != selectedBottomNav) {
-                            setState(() {
-                              selectedBottomNav = bottomNavs[index];
-                            });
-                          }
-                          Future.delayed(const Duration(seconds: 1), () {
-                            bottomNavs[index].input!.change(false);
-                          });
-                        },
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            AnimatedBar(
-                                isActive:
-                                    bottomNavs[index] == selectedBottomNav),
-                            SizedBox(
-                              height: 36,
-                              width: 36,
-                              child: Opacity(
-                                  opacity:
-                                      bottomNavs[index] == selectedBottomNav
-                                          ? 1
-                                          : 0.5,
-                                  child: RiveAnimation.asset(
-                                      bottomNavs[index].src,
-                                      artboard: bottomNavs[index].artboard,
-                                      onInit: (artboard) {
-                                    StateMachineController controller =
-                                        RiveUtils.getRiveController(artboard,
-                                            stateMachineName: bottomNavs[index]
-                                                .stateMachineName);
+          Transform(
+            alignment: Alignment.center,
+            transform: Matrix4.identity()
+              ..setEntry(3, 2, 0.001)
+              ..rotateY(animation.value - 30 * animation.value * pi / 180),
+            child: Transform.translate(
+              offset: Offset(animation.value * 265, 0),
+              child: Transform.scale(
+                scale: scaleAnimation.value,
+                child: const ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(24)),
+                  child: HomeScreen(),
+                ),
+              ),
+            ),
+          ),
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 200),
+            curve: Curves.fastOutSlowIn,
+            left: isSideMenuClosed ? 0 : 220,
+            top: 16,
+            child: MenuBtn(
+              riveOnInit: (artboard) {
+                StateMachineController controller = RiveUtils.getRiveController(
+                    artboard,
+                    stateMachineName: "State Machine");
+                isSideBarClosed = controller.findSMI("isOpen") as SMIBool;
+                isSideBarClosed.value = true;
+              },
 
-                                    bottomNavs[index].input =
-                                        controller.findSMI("active") as SMIBool;
-                                  })),
-                            )
-                          ],
-                        ),
-                      ))
-            ],
+              press: () {
+                isSideBarClosed.value = !isSideBarClosed.value;
+                if (isSideMenuClosed) {
+                  _animationController.forward();
+                } else {
+                  _animationController.reverse();
+                }
+                setState(() {
+                  isSideMenuClosed = isSideBarClosed.value;
+                });
+              },
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: Transform.translate(
+        offset: Offset(0, 100 * animation.value),
+        child: SafeArea(
+          child: Container(
+            padding: const EdgeInsets.all(12),
+            margin: const EdgeInsets.symmetric(horizontal: 24),
+            decoration: BoxDecoration(
+              color: backgroundColor2.withOpacity(0.8),
+              borderRadius: const BorderRadius.all(Radius.circular(24)),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ...List.generate(
+                    bottomNavs.length,
+                    (index) => GestureDetector(
+                          onTap: () {
+                            bottomNavs[index].input!.change(true);
+                            if (bottomNavs[index] != selectedBottomNav) {
+                              setState(() {
+                                selectedBottomNav = bottomNavs[index];
+                              });
+                            }
+                            Future.delayed(const Duration(seconds: 1), () {
+                              bottomNavs[index].input!.change(false);
+                            });
+                          },
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              AnimatedBar(
+                                  isActive:
+                                      bottomNavs[index] == selectedBottomNav),
+                              SizedBox(
+                                height: 36,
+                                width: 36,
+                                child: Opacity(
+                                    opacity:
+                                        bottomNavs[index] == selectedBottomNav
+                                            ? 1
+                                            : 0.5,
+                                    child: RiveAnimation.asset(
+                                        bottomNavs[index].src,
+                                        artboard: bottomNavs[index].artboard,
+                                        onInit: (artboard) {
+                                      StateMachineController controller =
+                                          RiveUtils.getRiveController(artboard,
+                                              stateMachineName: bottomNavs[index]
+                                                  .stateMachineName);
+
+                                      bottomNavs[index].input =
+                                          controller.findSMI("active") as SMIBool;
+                                    })),
+                              )
+                            ],
+                          ),
+                        ))
+              ],
+            ),
           ),
         ),
       ),

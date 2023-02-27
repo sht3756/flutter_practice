2979102dbe6b7713e65098e991158090251e9338
SHT-3756 <gkdlfndkekd@naver.com>
2023-02-27 17:15:05 +0900

    delete: 프로젝트 초기화

--- firebase_flutter_test/lib/main.dart
+++ firebase_flutter_test/lib/main.dart
@@ -1,53 +1,19 @@
-import 'package:firebase_flutter_test/layout/default_layout.dart';
-import 'package:firebase_flutter_test/screen/register_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-import 'screen/login_screen.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: HomePage(),
-    );
-  }
-}
-
-
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return DefaultLayout(
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => LoginScreen()));
-              },
-              child: const Text('로그인'),
-            ),
-            TextButton(onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) => RegisterScreen()));
-            }, child: Text('회원가입')),
-          ],
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text('홈'),
         ),
       ),
     );

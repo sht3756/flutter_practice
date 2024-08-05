378b5c1360075e01273e57d09f258060696fd6b7
SHT-3756 <gkdlfndkekd@naver.com>
2024-08-05 16:06:11 +0900

    feat : init 프로젝트 (assets, yaml)

---
+++ e_commerce_app/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: Scaffold(appBar: AppBar(), body: Center(child: Text('Hello World!'))),
+    );
+  }
+}

57d77a649da4bed0b34695d64a9a14ae4340af55
SHT-3756 <gkdlfndkekd@naver.com>
2024-08-06 18:46:49 +0900

    feat : Splash 개발

--- e_commerce_app/lib/presentation/pages/splash/component/splash_page.dart
+++ e_commerce_app/lib/presentation/pages/splash/component/splash_page.dart
@@ -1,20 +1,30 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class HomePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../../routes/route_path.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 2),() => context.go(RoutePath.home));
+  }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
-      body: Center(
-        child: Text(
-          'Home',
-          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-        ),
-      ),
+      body: Center(child: SvgPicture.asset('assets/svg/main_logo.svg')),
+      backgroundColor: Color(0xFF5F0080),
     );
   }
 }

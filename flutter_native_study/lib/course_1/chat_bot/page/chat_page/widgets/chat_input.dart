0dde65eef5a37673f150a5201d309fa0753e0f34
SHT-3756 <gkdlfndkekd@naver.com>
2024-09-16 23:49:54 +0900

    feat: 채팅방 인풋 공통 위젯 만들기

---
+++ flutter_native_study/lib/course_1/chat_bot/page/chat_page/widgets/chat_input.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class ChatInput extends StatefulWidget {
+  final Function(String message) onSend;
+  final bool isLoading;
+
+  const ChatInput({Key? key, required this.onSend, required this.isLoading})
+      : super(key: key);
+
+  @override
+  State<ChatInput> createState() => _ChatInputState();
+}
+
+class _ChatInputState extends State<ChatInput> {
+  final TextEditingController _controller = TextEditingController();
+
+  void _sendMessage() {
+    if (_controller.text.isNotEmpty) {
+      widget.onSend(_controller.text);
+      _controller.clear();
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        TextField(
+          controller: _controller,
+          decoration: const InputDecoration(hintText: 'Type a message ...'),
+        ),
+        widget.isLoading
+            ? Container(
+                margin: const EdgeInsets.all(8.0),
+                width: 24.0,
+                height: 24.0,
+                child: const CircularProgressIndicator(),
+              )
+            : IconButton(onPressed: _sendMessage, icon: const Icon(Icons.send)),
+      ],
+    );
+  }
+}

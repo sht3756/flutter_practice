284da7c55c8084346822f492e2c639bdbe300298
SHT-3756 <gkdlfndkekd@naver.com>
2024-09-17 00:00:16 +0900

    fix: 채팅방 인풋 공통 위젯 수정

--- flutter_native_study/lib/course_1/chat_bot/page/chat_page/widgets/chat_input.dart
+++ flutter_native_study/lib/course_1/chat_bot/page/chat_page/widgets/chat_input.dart
@@ -4,8 +4,7 @@ class ChatInput extends StatefulWidget {
   final Function(String message) onSend;
   final bool isLoading;
 
-  const ChatInput({Key? key, required this.onSend, required this.isLoading})
-      : super(key: key);
+  const ChatInput({super.key, required this.onSend, required this.isLoading});
 
   @override
   State<ChatInput> createState() => _ChatInputState();
@@ -29,21 +28,24 @@ class _ChatInputState extends State<ChatInput> {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        TextField(
-          controller: _controller,
-          decoration: const InputDecoration(hintText: 'Type a message ...'),
-        ),
-        widget.isLoading
-            ? Container(
-                margin: const EdgeInsets.all(8.0),
-                width: 24.0,
-                height: 24.0,
-                child: const CircularProgressIndicator(),
-              )
-            : IconButton(onPressed: _sendMessage, icon: const Icon(Icons.send)),
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          TextField(
+            controller: _controller,
+            decoration: const InputDecoration(hintText: 'Type a message ...'),
+          ),
+          widget.isLoading
+              ? Container(
+                  margin: const EdgeInsets.all(8.0),
+                  width: 24.0,
+                  height: 24.0,
+                  child: const CircularProgressIndicator(),
+                )
+              : IconButton(onPressed: _sendMessage, icon: const Icon(Icons.send)),
+        ],
+      ),
     );
   }
 }
